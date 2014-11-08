8dc8a6aa7ee0a9e3e1831ac2df9550ae21a82aa0
elmoremh <m.holly.elmore@gmail.com>
2014-11-08 15:00:14 -0500

    this script reads the filename and returns tylenol for odd numbered files and placebo for even numbered files

---
+++ assign_drug.py
@@ -0,0 +1,17 @@
+def assign_drug(filename):
+    number = filename[13:-4]
+    result = ''
+    if (int(number) % 2) == int(1):
+        result = 'tylenol'
+    else:
+        result = 'placebo'
+    return result
+
+import sys
+
+filename = sys.argv[1]
+print assign_drug(filename)
+
+assert assign_drug("inflammation_1.dat") == "tylenol"
+assert assign_drug("inflammation_4.dat") == "placebo"
+assert assign_drug("inflammation_3.dat") == "tylenol" 
